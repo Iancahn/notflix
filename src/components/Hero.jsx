a562775df68de2aef420587d341586ff05500f85
Ian Cahn <iancahn@live.com>
2022-08-26 12:13:45 +0200

    Update: Component Stylings

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
-//import FontAwesome Icons
+// import FontAwesome Icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faCircleInfo } from '@fortawesome/free-solid-svg-icons'
+// import images
+import fpb from '../img/fpb-icon.png';
+
 
 function Hero() {
     return (
@@ -11,11 +14,15 @@ function Hero() {
                     <h1>ME TIME</h1>
                 </div>
                 <div className="movie-subtitle">
-                    <p>In this buddy comedy, we see Mark and Kevin trying desperately to hold on to fame, and rake in some easy cash in the old tired duo comedy genre.</p>
+                    <p>In this buddy comedy, we see Mark W and Kevin H trying desperately to hold on to their fame, and rake in some easy cash in the old tired duo comedy genre.</p>
                 </div>
                 <div>
                     <button id="play-btn"><FontAwesomeIcon icon={faPlay} className="icon" /> Play</button>
                     <button id="more-info-btn"><FontAwesomeIcon icon={faCircleInfo} className="icon" /> More Info</button>
+                </div>
+                <div className="age-restrict">
+                    <img src={fpb} alt="you need to be older to watch this movie" id="fpb" />
+                    <p>16+</p>
                 </div>
             </section>
         </div>
