8b1ad5cbb5c51067ad62c530fe81e2ec71aeb4a6
Ian Cahn <iancahn@live.com>
2022-08-26 11:58:58 +0200

    Setup : Hero Section Done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+//import FontAwesome Icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faCircleInfo } from '@fortawesome/free-solid-svg-icons'
 
 function Hero() {
     return (
         <div className="hero">
-            <div>
-                <button>Learn More</button>
-                <button>Watch Now</button>
-            </div>
+            <section>
+                <div className="movie-title">
+                    <h1>ME TIME</h1>
+                </div>
+                <div className="movie-subtitle">
+                    <p>In this buddy comedy, we see Mark and Kevin trying desperately to hold on to fame, and rake in some easy cash in the old tired duo comedy genre.</p>
+                </div>
+                <div>
+                    <button id="play-btn"><FontAwesomeIcon icon={faPlay} className="icon" /> Play</button>
+                    <button id="more-info-btn"><FontAwesomeIcon icon={faCircleInfo} className="icon" /> More Info</button>
+                </div>
+            </section>
         </div>
     )
 }
